afc4c561482d3d9a98a806387e1e403d96a0a528
Maor Eini <maor.eini@gmail.com>
2017-05-03 17:07:03 +0300

    clean up boilerplate project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/default */
 
 import React from 'react';
 import { render } from 'react-dom';
@@ -7,15 +6,13 @@ import { AppContainer } from 'react-hot-loader';
 import Root from './components/Root';
 
 import configureStore from './store/configureStore';
-require('./favicon.ico'); // Tell webpack to load favicon.ico
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+require('./favicon.ico');
+import './styles/styles.scss';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
 
-// Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
-
 
 render(
   <AppContainer>
